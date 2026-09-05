0b17525e29714e890bd490b14327064a9a67f2be
Suryansh Pandey <suryanshpandeywork@gmail.com>
2026-09-05 17:27:07 +0530

    feat(worker): add ConfirmDone with local retry

--- pkg/worker/server.go
+++ pkg/worker/server.go
@@ -6,16 +6,28 @@ import (
 	"sync/atomic"
 
 	pb "github.com/Suryansh3105/taskmaster/pkg/grpcapi"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Server struct {
 	pb.UnimplementedTaskServiceServer
-	config       Config
-	runningTasks int32
+	config          Config
+	runningTasks    int32
+	coordinatorConn *grpc.ClientConn
+	coordinatorClient pb.TaskServiceClient
 }
 
-func NewServer(config Config) *Server {
-	return &Server{config: config}
+func NewServer(config Config) (*Server, error) {
+	conn, err := grpc.NewClient(config.CoordinatorAddresses[0], grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	return &Server{
+		config:            config,
+		coordinatorConn:   conn,
+		coordinatorClient: pb.NewTaskServiceClient(conn),
+	}, nil
 }
 
 func (s *Server) RunningTasks() int32 {
@@ -36,6 +48,7 @@ func (s *Server) AssignTask(ctx context.Context, req *pb.AssignTaskRequest) (*pb
 		} else {
 			log.Printf("worker %s: task %s failed: %s", s.config.WorkerID, req.TaskId, result.ErrorMessage)
 		}
+		ConfirmDone(context.Background(), s.coordinatorClient, req.TaskId, result)
 	}()
 
 	return &pb.AssignTaskResponse{Accepted: true}, nil
